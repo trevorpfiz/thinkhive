23918657178f8732e587f65256f34265274de751
ElektrikSpark <elektrikspark@gmail.com>
2023-03-30 21:54:13 -0500

    upload usage delta bar

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -131,4 +131,26 @@ export const userRouter = createTRPCRouter({
       },
     ];
   }),
+  getUserCredits: protectedProcedure.query(async ({ ctx }) => {
+    const { session, prisma } = ctx;
+
+    if (!session.user?.id) {
+      throw new Error('Not authenticated');
+    }
+
+    const data = await prisma.user.findUnique({
+      where: {
+        id: session.user?.id,
+      },
+      select: {
+        credits: true,
+      },
+    });
+
+    if (!data) {
+      throw new Error('Could not find user');
+    }
+
+    return parseFloat(data.credits.toFixed(2));
+  }),
 });
