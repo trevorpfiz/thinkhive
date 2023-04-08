56d7460150f9454732313fa50547641a68d794d2
Zachary <ztpfizcode@gmail.com>
2023-04-07 20:48:31 -0500

    console log sources

--- src/components/widget/ChatInput.tsx
+++ src/components/widget/ChatInput.tsx
@@ -63,10 +63,10 @@ export default function ChatInput({
       expertId,
     });
     setLoading(false);
-    // console.log(response.sourceDocuments[0].pageContent);
-    // console.log(response.sourceDocuments[1].pageContent);
-    // console.log(response.sourceDocuments[2].pageContent);
-    // console.log(response.sourceDocuments[3].pageContent);
+    console.log(response.sourceDocuments[0].pageContent);
+    console.log(response.sourceDocuments[1].pageContent);
+    console.log(response.sourceDocuments[2].pageContent);
+    console.log(response.sourceDocuments[3].pageContent);
 
     if (response.text) {
       console.log(response.text);
